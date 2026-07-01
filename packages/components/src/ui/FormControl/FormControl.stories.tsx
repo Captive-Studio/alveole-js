04818f21de259671f579cf253867ad5dd267acf4
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-07-01 11:25:06 +0200

    📝 Passe la doc de FormControl avec le tag ui plutot que core

--- packages/components/src/ui/FormControl/FormControl.stories.tsx
+++ packages/components/src/ui/FormControl/FormControl.stories.tsx
@@ -16,7 +16,7 @@ import { TextInput } from './TextInput';
 
 export default {
   title: 'FormControl',
-  tags: ['core'],
+  tags: ['ui'],
   experimental: false,
   description: 'Eléments de construction de champs de saisie (label, input, helper text).',
   component: FormControl,
