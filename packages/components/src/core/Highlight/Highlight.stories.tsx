d4dfc167e8bca3adfc5e3e90558f973f1cb1bd93
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-07-28 10:38:13 +0200

    📝 Ajoute un lien sur la story de HIghlight

--- packages/components/src/core/Highlight/Highlight.stories.tsx
+++ packages/components/src/core/Highlight/Highlight.stories.tsx
@@ -6,7 +6,7 @@ export default {
   title: 'Highlight',
   tags: ['core'],
   experimental: true,
-  description: 'Affiche du code en typescript, html, json, etc. Composant de type Light (react-syntax-highlighter).',
+  description: 'Affiche du code en typescript, html, json, etc. Composant de type Light ([react-syntax-highlighter](https://github.com/react-syntax-highlighter/react-syntax-highlighter)).',
   component: Highlight,
   styleFn: useStyles,
 } satisfies Story;
