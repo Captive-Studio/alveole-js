c679d4f63ffa23bd2a4c227062e331c83445ccaf
Corentin Couq <corentin.couq@captive.fr>
2026-08-11 16:58:17 +0200

    ✨ Export le RadioInput

--- packages/components/src/ui/RadioGroup/index.ts
+++ packages/components/src/ui/RadioGroup/index.ts
@@ -1,6 +1,7 @@
 import { RadioGroup as RadioGroupComponent } from './RadioGroup';
 import { RadioGroupCard } from './RadioGroupCard';
 import { RadioGroupItem } from './RadioGroupItem';
+import { RadioInput } from './RadioInput';
 
 export { RadioGroupElement, RadioGroupProps } from './RadioGroup';
 export { RadioGroupCardProps } from './RadioGroupCard';
@@ -10,4 +11,5 @@ export { RadioGroupItemProps } from './RadioGroupItem';
 export const RadioGroup = Object.assign(RadioGroupComponent, {
   Item: RadioGroupItem,
   Card: RadioGroupCard,
+  Input: RadioInput,
 });
